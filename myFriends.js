a964e6e81fd88c777392776601bc336940c45214
moogleKupo <tmaine@gmx.com>
2023-06-18 05:34:02 +0000

    friend filter based on 4 characters

---
+++ myFriends.js
@@ -0,0 +1,12 @@
+function friend(friends){
+  //your code here
+  let myFriends = [];
+  for (i = 0; i < friends.length; i++){
+    if (friends[i].length === 4){
+      myFriends.push(friends[i]);
+    }
+  }
+  return myFriends;
+}
+// TEST
+console.log(friend(["mark", "taylor", "prescilla", "brad"]));
