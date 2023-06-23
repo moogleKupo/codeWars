e86e08405e58bdfe689c030212571ed131250e76
moogleKupo <tmaine@gmx.com>
2023-06-23 18:59:57 +0000

    bus passengers

---
+++ busPassengers.js
@@ -0,0 +1,18 @@
+let gotOn = 0;
+let gotOff = 0;
+// find out how many are added, how many get off and update result
+const passengerRemainderCalc = function(obj) {
+  for (let key in obj) {
+   gotOn += parseInt(key);
+   gotOff += obj[key];
+  }
+  let result = gotOn - gotOff;  
+  return  result;
+  };
+  
+
+console.log(passengerRemainderCalc({
+  3: 1,
+  4: 6,
+  7: 5
+}))
