735c0afe2bf3cfc0a090f88d749679b31d82348b
moogleKupo <tmaine@gmx.com>
2023-06-23 04:11:00 +0000

    highandLow work in progress

--- highAndLow.js
+++ highAndLow.js
@@ -1,6 +1,11 @@
-const highAndLow = function(numbers){
-  let low = Math.floor(numbers);
-  let high = Math.ceil(numbers);
-  return `(${low}, ${high})`
+function highAndLow(numbers){
+  // ...
+  let highNum = 0;
+  let lowNUm = 0;
+  for (let i = 0; i < numbers.length; i++){
+    if (numbers[i] > (numbers[i + 2] {
+        highNum = numbers[i];
+        } else {highNum = numbers[i + 2];
+  }
 }
-console.log(highAndLow(1, 2))
+}
